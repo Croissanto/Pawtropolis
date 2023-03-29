cd46a048008f8719db80975a82feb903fc90f13b
Croissanto <antoniopapaleo24@gmail.com>
2023-03-29 15:52:16 +0200

    Create Animal.java

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Date;
+
+public class Animal {
+
+    String name;
+    String favoriteFood;
+    int age;
+    Date zooEnteringDate;
+    double weight;
+    double height;
+    
+
+
+}
